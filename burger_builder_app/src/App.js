c5693f039b93be3efd7e4421793a27e30287b9b0
Ritika Gupta <guptaritika782@gmail.com>
2021-03-04 20:45:57 +0530

    Structured BurgerBuilder Container

--- burger_builder_app/src/App.js
+++ burger_builder_app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Layout />
+        <BurgerBuilder />
       </div>
     );
   }
