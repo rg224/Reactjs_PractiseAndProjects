c2e73c85b5d827c71221bba9206c70ccba577b04
Ritika Gupta <guptaritika782@gmail.com>
2021-03-07 21:51:41 +0530

    Added Burger Component

--- burger_builder_app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger_builder_app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,24 +7,28 @@ class BurgerIngredient extends Component {
   render () {
     let ingredient = null;
 
-    switch (props.types) {
+    switch (this.props.type) {
       case ('bread-top'):
-        ingredient = <div className={classes.BreadTop}></div>;
+        ingredient = 
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>;
         break;
       case ('bread-bottom'):
-        ingredient = <div lassName={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.BreadBottom}></div>;
         break;
       case ('salad'):
-        ingredient = <div lassName={classes.Salad}></div>;
+        ingredient = <div className={classes.Salad}></div>;
         break;
       case ('meat'):
-        ingredient = <div lassName={classes.Meat}></div>;
+        ingredient = <div className={classes.Meat}></div>;
         break;
       case ('bacon'):
-        ingredient = <div lassName={classes.Bacon}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       case ('cheese'):
-        ingredient = <div lassName={classes.Cheese}></div>;
+        ingredient = <div className={classes.Cheese}></div>;
         break;
       default :
         ingredient=null;
