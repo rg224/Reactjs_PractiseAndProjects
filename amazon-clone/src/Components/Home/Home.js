cef69581615b81b6a6fdf761e12503e1eefbdcaf
Ritika Gupta <guptaritika782@gmail.com>
2021-03-27 13:02:50 +0530

    Added Banner to HomePage

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const home = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <HomeContainer>
+      <Banner></Banner>
+    </HomeContainer>
   );
 }
 
 export default home;
+
+const HomeContainer = styled.div ``
+
+const Banner = styled.div `
+  background-image: url('https://i.imgur.com/SYHeuYM.jpg');
+  min-height: 600px;
+  background-position: center;
+  background-size: cover;
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+` 
