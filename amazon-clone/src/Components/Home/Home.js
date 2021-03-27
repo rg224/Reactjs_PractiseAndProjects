c4bcd688928a6d40b70efd349710445c69bc597d
Ritika Gupta <guptaritika782@gmail.com>
2021-03-27 16:02:15 +0530

    Added Content part in HomePage

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -5,18 +5,33 @@ const home = () => {
   return (
     <HomeContainer>
       <Banner></Banner>
+
+      <Content>
+        CONTENT PART
+      </Content>
     </HomeContainer>
   );
 }
 
 export default home;
 
-const HomeContainer = styled.div ``
+// adjust max-width, min-height, margin-top according to your acreen size
+const HomeContainer = styled.div `
+  max-width: 1200px;
+  margin: 0 auto;
+`
 
 const Banner = styled.div `
   background-image: url('https://i.imgur.com/SYHeuYM.jpg');
-  min-height: 600px;
+  min-height: 450px;
   background-position: center;
   background-size: cover;
   mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+` 
+
+const Content = styled.div `
+  background: white;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-top: -260px;
 ` 
